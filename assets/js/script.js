f5bbda33f9565653f73b1b2b198d0a97b5dd3d3f
Harpreet-Bambrah <hs289523@gmail.com>
2024-10-07 10:44:21 +0530

    harpreetdev portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,32 +14,4 @@ const popupMenu =  () => {
     bar[1].classList.toggle("rotate_bar3");
     bar[2].classList.toggle("rotate_bar2");
 };
-// contact_form_data
-let data={};
-document.querySelectorAll("#hd_contact_form input,textarea").forEach((ele)=>{
-    ele.addEventListener('input',(e)=>{    
-        console.log(data)       
-            data={
-                ...data,[e.target.name]:e.target.value
-            }         
-        })
-})
-document.querySelector("#hd_contact_form").addEventListener('submit', function(e){
-    e.preventDefault()
-   const FormBtn  = document.querySelector('#form_btn_loading')
-    FormBtn.classList.add('form_btn')  
-    fetch("https://portfolio-8878.onrender.com/message", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
-        .then((response) => response.json())
-        .then((json) =>{
-            // setbtnvalue('Message')
-            document.querySelectorAll('form > .form_input').forEach((ele)=>{ele.value=''})  
-            FormBtn.classList.remove('form_btn')              
-            alert('Your message has been successfully sent')            
-        });
-})
+
